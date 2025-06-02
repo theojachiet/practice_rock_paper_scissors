bf9dc5f884ec9a7eb0ae0d61da7eb940c8f45663
Teoslip <tjachiet@gmail.com>
2025-06-02 10:04:54 +0200

    Add basic functions for user and computer choices

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log('test');
+function getComputerChoice() {
+  let randomNumber = Math.floor(Math.random() * 3) + 1;
+  if (randomNumber === 1) {
+    return 'rock';
+  } else if (randomNumber === 2) {
+    return 'paper';
+  } else {
+    return 'scissors';
+  }
+}
+
+function getUserChoice(input) {
+
+  if (input == null || input == undefined) {
+    console.log('You must input a valid option');
+  }
+
+  let userChoice = input.toLowerCase();
+
+  if (userChoice != 'rock' || userChoice != 'paper' || userChoice != 'scissors')
+
+  return userChoice;
+}
+
+function getUserInput() {
+  let userInput = prompt('Ready ? Type your choice !');
+  return userInput;
+}
+
+function playRound() {
+  let userInput = getUserInput();
+  getUserChoice(userInput);
+  getComputerChoice();
+}
+
+playRound();
